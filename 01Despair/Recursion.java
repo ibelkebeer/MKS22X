56eeaeb90d36b5a598909d8ba69e7caa809346c1
Imad <ibelkebir@stuy.edu>
2018-02-05 23:22:05 -0500

    recursive functions

---
+++ 01Despair/Recursion.java
@@ -0,0 +1,42 @@
+public class Recursion{
+    public int fact(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if(n == 0){
+	    return 1;
+	}
+	return n * fact(n-1);
+    }
+    public int fib(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if(n<=1){
+	    return n;
+	}
+	return fibHelp(n,2,1,1);
+    }
+    public int fibHelp(int n, int next, int sum, int last){
+	int tempLast = sum;
+	sum += last;
+	last = tempLast;
+	if(next == n){
+	    return sum;
+	}
+	return fibHelp(n, next+1, sum, last);
+    }
+    public double sqrt(double n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return sqrtHelp(n, n/2);
+    }
+    public double sqrtHelp(double n, double g){
+	double percentError = ((g*g)-n)/n*100;
+	if(percentError < 0.0000001 && percentError > -0.0000001){
+	    return g;
+	}
+	return sqrtHelp(n,(n/g + g) / 2);
+    }	
+}
