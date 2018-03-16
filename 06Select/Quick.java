f84f47a9ad56a28b5e2e4c1e04e51cfa0d87cfc2
Imad <ibelkebir@stuy.edu>
2018-03-15 22:40:35 -0400

    Added quicksort, quickselect uses dutch flag

--- 06Select/Quick.java
+++ 06Select/Quick.java
@@ -1,39 +1,79 @@
 import java.util.*;
 public class Quick{
-    public static int partition(int[] data, int start, int end){
+    public static void swap(int[] data,int x,int y){
+	int temp = data[x];
+	data[x] = data[y];
+	data[y] = temp;
+    }
+    public static void partition(int[] data, int start, int end){
 	int pivotIndex = (int)(Math.random() * (end - start + 1) + start);
 	int pivot = data[pivotIndex];
 	int first = start;
-	data[pivotIndex] = data[start];
-	data[start] = pivot;
-	start++;
-	while(start <= end){
-	    if(data[start] < pivot){
-		start++;
+        swap(data,pivotIndex,start);
+	int lt = start;
+	int i = start+1;
+	int gt = end;
+	while(i <= gt){
+	    if(data[i] == pivot){
+		i++;
 	    }
-	    else if(data[start] > pivot){
-		int temp = data[end];
-		data[end] = data[start];
-		data[start] = temp;
-		end--;
+	    else if(data[i] > pivot){
+		swap(data,i,gt);
+		gt--;
+	    }
+	    else{
+		swap(data,i,lt);
+		lt++;
+		i++;
 	    }
 	}
-	data[first] = data[end];
-	data[end] = pivot;
-	return end;
+	if(start < end-1){
+	    if(i < data.length){
+		partition(data,i,end);	    
+	    }
+	    partition(data,start,lt-1);
+	}
+    }
+    public static int[] partition2(int[] data, int start, int end){
+	int pivotIndex = (int)(Math.random() * (end - start) + start);
+	int pivot = data[pivotIndex];
+	int first = start;
+        swap(data,pivotIndex,start);
+	int lt = start;
+	int i = start+1;
+	int gt = end;
+	while(i <= gt){
+	    if(data[i] == pivot){
+		i++;
+	    }
+	    else if(data[i] > pivot){
+		swap(data,i,gt);
+		gt--;
+	    }
+	    else{
+		swap(data,i,lt);
+		lt++;
+		i++;
+	    }
+	}
+        int[] range = {lt,gt};
+	return range;
     }
     public static int quickselect(int[] data, int k){
 	int start = 0;
 	int end = data.length - 1;
-	int v = partition(data,start,end);
-	while(v != k){
-	    if(v < k){
-		start = v+1;
+	int[] v = partition2(data,start,end);
+	while(v[0] > k || v[1] < k){
+	    if(v[1] < k){
+		start = v[1]+1;
 	    }else{
-		end = v;
+		end = v[0]-1;
 	    }
-	    v = partition(data,start,end);
+	    v = partition2(data,start,end);
 	}
-	return data[v];
+	return data[v[0]];
+    }
+    public static void quicksort(int[] data){
+	partition(data,0,data.length-1);
     }
 }
