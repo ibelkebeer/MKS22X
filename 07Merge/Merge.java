05838856bddde4dfe3f685fb1edd44af169c4e2c
Imad <ibelkebir@stuy.edu>
2018-03-22 14:19:06 -0400

    Removed unecessary code

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class Merge{
     public static void mergesort(int[]data){
 	msort(data,0,data.length-1);
@@ -12,15 +11,12 @@ public class Merge{
 	merge(data,lo,hi);
     }
     public static void merge(int[]data,int lo,int hi){
-	//System.out.println(Arrays.toString(data)+","+lo+","+hi);
 	int mid = (hi+lo)/2 + 1;
-	int cur = mid;
 	while(lo < mid){
-	    if(data[cur] < data[lo]){
-		swap(data,lo,cur);
+	    if(data[mid] < data[lo]){
+		swap(data,lo,mid);
 		if(mid < hi){
 		    mid++;
-		    cur++;
 		}
 		
 	    }
@@ -33,24 +29,5 @@ public class Merge{
 	    data[x] = data[x-1];
 	}
 	data[start] = temp;
-    }
-    public static void main(String[]args){
-        /*int[] test = new int[] {3,9,1,4,1,5,7,9,3,12,11,3,6,8,2};
-	mergesort(test);
-	*/
-	
-	int[] test = new int[1000];
-	for(int x=0; x<1000; x++){
-	    test[x] = (int)(Math.random() * 1000);
-	}
-	mergesort(test);
-	boolean sorted = true;
-	for(int x=0; x<999; x++){
-	    if(test[x] > test[x+1]){
-		sorted = false;
-	    }
-	}
-	
-	System.out.println(sorted);
     }
 }
