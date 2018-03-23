5209a6bf0e349cf3be2866e83ae8ae239ffd0086
Imad <ibelkebir@stuy.edu>
2018-03-22 22:35:53 -0400

    Fixed merge, uses extra array

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,33 +1,45 @@
+import java.util.*;
 public class Merge{
     public static void mergesort(int[]data){
-	msort(data,0,data.length-1);
+	int[] temp = new int[data.length];
+	msort(data,temp,0,data.length-1);
     }
-    public static void msort(int[]data,int lo,int hi){
-	if(hi-lo < 1){
+    public static void msort(int[]data,int[] temp,int lo,int hi){
+	if(lo >= hi){
 	    return;
 	}
-	msort(data,lo,(hi+lo)/2);
-	msort(data,(hi+lo)/2+1,hi);
-	merge(data,lo,hi);
-    }
-    public static void merge(int[]data,int lo,int hi){
-	int mid = (hi+lo)/2 + 1;
-	while(lo < mid){
-	    if(data[mid] < data[lo]){
-		swap(data,lo,mid);
-		if(mid < hi){
-		    mid++;
-		}
-		
-	    }
-	    lo++;
-        }
-    }
-    public static void swap(int[]data,int start,int end){
-	int temp = data[end];
-	for(int x=end; x>start; x--){
-	    data[x] = data[x-1];
+	for(int x=0; x<data.length; x++){
+	    temp[x] = data[x];
 	}
-	data[start] = temp;
+	int mid = (hi+lo)/2;
+	msort(temp,data,lo,mid);
+	msort(temp,data,mid+1,hi);
+	merge(data,temp,lo,mid,mid+1,hi);
+    }
+    public static void merge(int[]data,int[]temp,int lo1,int hi1,int lo2,int hi2){
+        int cur = lo1;
+	while(cur <= hi2){
+	    if(lo1 > hi1){
+		while(lo2 <= hi2){
+		    data[cur] = temp[lo2];
+		    cur++;
+		    lo2++;
+		}
+	    }else if(lo2 > hi2){
+		while(lo1 <= hi1){
+		    data[cur] = temp[lo1];
+		    cur++;
+		    lo1++;
+		}
+	    }else if(temp[lo1] <= temp[lo2]){
+		data[cur] = temp[lo1];
+		cur++;
+		lo1++;
+	    }else{
+		data[cur] = temp[lo2];
+		cur++;
+		lo2++;
+	    }
+	}
     }
 }
