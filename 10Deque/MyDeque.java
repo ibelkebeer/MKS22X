6fe957d905b9e2a392f8ce65d4270863cc6a56aa
Imad <ibelkebir@stuy.edu>
2018-04-18 21:29:55 -0400

    Added up to get methods

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<E>{
     private E[]ary;
     private int start;
@@ -22,18 +23,28 @@ public class MyDeque<E>{
     }
     public String toString(){
 	String fin = "[";
-	for(int i=0; i<size; i++){
-	    fin += ary[i] + " ";
+	int temp = start+1;
+	for(int i=0; i<size-1; i++){
+	    fin += ary[temp % ary.length] + ",";
+	    temp++;
 	}
-	if(size != 0){
-	    fin += ary[size-1] + "]";
+	if(end == 0 && size > 0){
+	    fin += ary[temp % ary.length];
+	}else if(size > 0){
+	    fin += ary[end-1];
 	}
-	return fin;
+	return fin + "]";
     }
     public void addFirst(E element){
-	if(start >= end){
+	if(element == null){
+	    throw new NullPointerException();
+	}
+	if(ary[start] == null){
 	    ary[start] = element;
 	    start--;
+	    if(start < 0){
+		start += ary.length;
+	    }
 	    size++;
 	}else{
 	    start++;
@@ -44,11 +55,16 @@ public class MyDeque<E>{
 	}
     }
     public void addLast(E element){
-	if(end <= start){
+	if(element == null){
+	    throw new NullPointerException();
+	}
+	if(ary[end] == null){
 	    ary[end] = element;
 	    end++;
+	    end = end % ary.length;
 	    size++;
 	}else{
+	    start++;
 	    resize();
 	    ary[end] = element;
 	    end++;
@@ -57,20 +73,69 @@ public class MyDeque<E>{
     }
     @SuppressWarnings("unchecked")
     public void resize(){
-	E[] bigger = (E[])new Object[size + 10]; 
-	for(int i=size; i<size+10; i++){
-	    bigger[i] = ary[start % size];
-	    size++;
+	E[] bigger = (E[])new Object[size + 10];
+	int temp = 10;
+	for(int i=0; i<size; i++){
+	    bigger[temp] = ary[start % size];
+	    start++;
+	    temp++;
 	}
 	ary = bigger;
 	end = 0;
 	start = 9;
     }
-    public static void main(String[]args){
-	MyDeque<Integer> test = new MyDeque<>();
-	for(int x=0; x<1; x++){
-	    test.addFirst(new Integer(5));
+    public E removeFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
 	}
-	System.out.println(test);
+	E temp;
+	if(start == ary.length-1){
+	    temp = ary[0];
+	    ary[0] = null;
+	    start = 0;
+	    size--;
+	}else{
+	    start++;
+	    temp = ary[start];
+	    ary[start] = null;
+	    size--;
+	}
+	return temp;
+    }
+    public E removeLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	E temp;
+	if(end == 0){
+	    temp = ary[ary.length - 1];
+	    ary[ary.length - 1] = null;
+	    end = ary.length - 1;
+	    size--;
+	}else{
+	    end--;
+	    temp = ary[end];
+	    ary[end] = null;
+	    size--;
+	}
+	return temp;
+    }
+    public E getFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	if(start == ary.length - 1){
+	    return ary[0];
+	}
+	return ary[start+1];
+    }
+    public E getLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	if(end == 0){
+	    return ary[ary.length - 1];
+	}
+	return ary[end-1];
     }
 }
